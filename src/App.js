c88cfd350f89adb624c539a78e9c0e592798d55a
Hari_karthyk <Harikarthyk@users.noreply.github.com>
2021-01-30 21:10:08 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {GoogleMap, LoadScript} from "@react-google-maps/api";
+import "./App.css";
+require("dotenv").config();
+/*  */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const mapStyles = {
+	height: "100vh",
+	width: "100%",
+};
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			location: {
+				lat: 41.3851,
+				lng: 2.1734,
+			},
+		};
+	}
+
+	componentDidMount() {
+		navigator.geolocation.getCurrentPosition(
+			(position) => {
+				console.log("Pos ", position);
+				this.setState({
+					location: {
+						lat: position.coords.latitude,
+						lng: position.coords.longitude,
+					},
+				});
+			},
+			(error) => {
+				alert(JSON.stringify(error));
+			},
+			{
+				enableHighAccuracy: true,
+				timeout: 20000,
+				maximumAge: 1000,
+			},
+		);
+	}
+	render() {
+		return (
+			<>
+				<LoadScript googleMapsApiKey='AIzaSyDejMEw7iAaAFt7QvmHDhiY1NpZK7R-MRw&'>
+					<GoogleMap
+						mapContainerStyle={mapStyles}
+						zoom={16}
+						center={this.state.location}
+					/>
+				</LoadScript>
+				{console.log(this.state.location)}
+			</>
+		);
+	}
 }
 
 export default App;
