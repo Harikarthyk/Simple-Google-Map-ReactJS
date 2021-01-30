6d543340a1688f27b5e976f658ab3749e2a35430
Hari_karthyk <Harikarthyk@users.noreply.github.com>
2021-01-30 21:47:51 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
-import {GoogleMap, LoadScript} from "@react-google-maps/api";
+import {GoogleMap, LoadScript, Marker} from "@react-google-maps/api";
 import "./App.css";
+import Geocode from "react-geocode";
 
 const mapStyles = {
 	height: "100vh",
 	width: "100%",
 };
+
+Geocode.setApiKey("AIzaSyDejMEw7iAaAFt7QvmHDhiY1NpZK7R-MRw&");
+Geocode.setLanguage("en");
+Geocode.setRegion("in");
+Geocode.enableDebug();
 
 class App extends React.Component {
 	constructor(props) {
@@ -17,17 +23,34 @@ class App extends React.Component {
 			},
 		};
 	}
-
+	getAddress = () => {
+		Geocode.fromLatLng(this.state.location.lat, this.state.location.lng).then(
+			(response) => {
+				const address = response.results[0].formatted_address;
+				console.log(address);
+			},
+			(error) => {
+				console.error(error);
+			},
+		);
+	};
+	onMarkerDragEnd = (e) => {
+		console.log(e);
+		const lat = e.latLng.lat();
+		const lng = e.latLng.lng();
+		this.setState({location: {lat, lng}});
+		this.getAddress();
+	};
 	componentDidMount() {
 		navigator.geolocation.getCurrentPosition(
 			(position) => {
-				console.log("Pos ", position);
 				this.setState({
 					location: {
 						lat: position.coords.latitude,
 						lng: position.coords.longitude,
 					},
 				});
+				this.getAddress();
 			},
 			(error) => {
 				alert(JSON.stringify(error));
@@ -47,7 +70,13 @@ class App extends React.Component {
 						mapContainerStyle={mapStyles}
 						zoom={16}
 						center={this.state.location}
-					/>
+					>
+						<Marker
+							position={this.state.location}
+							draggable={true}
+							onDragEnd={(e) => this.onMarkerDragEnd(e)}
+						/>
+					</GoogleMap>
 				</LoadScript>
 				{console.log(this.state.location)}
 			</>
