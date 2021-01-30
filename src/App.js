01c0610060e6da2ff8769ebc6d117929201aef7c
Hari_karthyk <Harikarthyk@users.noreply.github.com>
2021-01-30 23:09:36 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import {GoogleMap, LoadScript, Marker} from "@react-google-maps/api";
+import PlacesAutocomplete, {
+	geocodeByAddress,
+	getLatLng,
+} from "react-places-autocomplete";
+
 import "./App.css";
 import Geocode from "react-geocode";
 
 const mapStyles = {
-	height: "100vh",
+	height: "80vh",
 	width: "100%",
 };
 
@@ -21,8 +26,26 @@ class App extends React.Component {
 				lat: 41.3851,
 				lng: 2.1734,
 			},
+			address: "",
 		};
 	}
+	handleChange = (newAddress) => {
+		this.setState({location: this.state.location, address: newAddress});
+	};
+
+	handleSelect = (newAddress) => {
+		this.setState({location: this.state.location, address: newAddress});
+		geocodeByAddress(newAddress)
+			.then((results) => getLatLng(results[0]))
+			.then((latLng) => {
+				console.log("Success", latLng);
+
+				// this.setState({location: latLng, address: this.state.address});
+				// update center state
+				// this.setState({mapCenter: latLng});
+			})
+			.catch((error) => console.error("Error", error));
+	};
 	getAddress = () => {
 		Geocode.fromLatLng(this.state.location.lat, this.state.location.lng).then(
 			(response) => {
@@ -38,7 +61,7 @@ class App extends React.Component {
 		console.log(e);
 		const lat = e.latLng.lat();
 		const lng = e.latLng.lng();
-		this.setState({location: {lat, lng}});
+		this.setState({location: {lat, lng}, address: this.state.address});
 		this.getAddress();
 	};
 	componentDidMount() {
@@ -65,7 +88,10 @@ class App extends React.Component {
 	render() {
 		return (
 			<>
-				<LoadScript googleMapsApiKey='AIzaSyDejMEw7iAaAFt7QvmHDhiY1NpZK7R-MRw&'>
+				<LoadScript
+					// onLoad={this.handleScriptLoad}
+					googleMapsApiKey='AIzaSyDejMEw7iAaAFt7QvmHDhiY1NpZK7R-MRw&libraries=places'
+				>
 					<GoogleMap
 						mapContainerStyle={mapStyles}
 						zoom={16}
@@ -77,8 +103,51 @@ class App extends React.Component {
 							onDragEnd={(e) => this.onMarkerDragEnd(e)}
 						/>
 					</GoogleMap>
+					<PlacesAutocomplete
+						value={this.state.address}
+						onChange={this.handleChange}
+						onSelect={this.handleSelect}
+					>
+						{({
+							getInputProps,
+							suggestions,
+							getSuggestionItemProps,
+							loading,
+						}) => (
+							<div>
+								<input
+									{...getInputProps({
+										placeholder: "Search Places ...",
+										className: "location-search-input",
+									})}
+								/>
+								<div className='autocomplete-dropdown-container'>
+									{/* {loading && <div>Loading...</div>} */}
+									{suggestions.map((suggestion) => {
+										const className = suggestion.active
+											? "sugg estion-item--active"
+											: "suggestion-item";
+										// inline style for demonstration purpose
+										const style = suggestion.active
+											? {backgroundColor: "#fafafa", cursor: "pointer"}
+											: {backgroundColor: "#ffffff", cursor: "pointer"};
+										return (
+											<div
+												{...getSuggestionItemProps(suggestion, {
+													className,
+													style,
+												})}
+											>
+												<span>{suggestion.description}</span>
+											</div>
+										);
+									})}
+								</div>
+							</div>
+						)}
+					</PlacesAutocomplete>
 				</LoadScript>
-				{console.log(this.state.location)}
+				{/* {console.log(this.state.location)} */}
 			</>
 		);
 	}
